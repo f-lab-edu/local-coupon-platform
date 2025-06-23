07506715e7193c3428f4e73509d7250df513d15d
sdj3261 <tlsehdwo7@naver.com>
2025-06-23 16:22:20 +0900

    feat: 회원가입 및 내 정보 조회 API 구현 feat: PasswordEncoder Argon2적용
    - 회원가입 API 구현 및 테스트 작성
    - 내 정보 조회 API 및 RestDocs 문서화
    - 비밀번호 암호화 로직 추가

---
+++ src/main/java/com/localcoupon/couponservice/user/enums/UserErrorCode.java
@@ -0,0 +1,32 @@
+package com.localcoupon.couponservice.user.enums;
+
+import com.localcoupon.couponservice.global.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+public enum UserErrorCode implements ErrorCode {
+    USER_NOT_FOUND("유저를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    USER_ALREADY_EXISTS("이미 존재하는 유저입니다.", HttpStatus.BAD_REQUEST);
+
+    private final String message;
+    private final HttpStatus status;
+
+    UserErrorCode(String message, HttpStatus status) {
+        this.message = message;
+        this.status = status;
+    }
+
+    @Override
+    public String getCode() {
+        return this.name();
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public HttpStatus getStatus() {
+        return status;
+    }
+}
