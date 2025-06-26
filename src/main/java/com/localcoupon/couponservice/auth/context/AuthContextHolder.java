1852f96596913e4f86dd29a85427403cf538bee5
sdj3261 <tlsehdwo7@naver.com>
2025-06-26 15:22:58 +0900

    refactor : AuthInterceptor preHandle 쓰기 작업 리펙토링, LogContext 파라미터 리펙토링

--- src/main/java/com/localcoupon/couponservice/auth/context/AuthContextHolder.java
+++ src/main/java/com/localcoupon/couponservice/auth/context/AuthContextHolder.java
@@ -2,6 +2,7 @@ package com.localcoupon.couponservice.auth.context;
 
 
 public class AuthContextHolder {
+    private static final String NO_AUTH = "NO_AUTH";
     private static final ThreadLocal<String> userIdHolder = new ThreadLocal<>();
 
     public static void setUserKey(String userId) {
@@ -9,7 +10,7 @@ public class AuthContextHolder {
     }
 
     public static String getUserKey() {
-        return userIdHolder.get();
+        return userIdHolder.get() != null ? userIdHolder.get() : NO_AUTH;
     }
 
     public static void clear() {
