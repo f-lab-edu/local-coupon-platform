9444d89cdefdfc209fb758b237ada355b2684490
sdj3261 <tlsehdwo7@naver.com>
2025-07-03 15:05:19 +0900

    [#5] fix : Store BaseEntity 상속 오류 수정

--- src/main/java/com/localcoupon/couponservice/store/repository/StoreRepository.java
+++ src/main/java/com/localcoupon/couponservice/store/repository/StoreRepository.java
@@ -4,11 +4,15 @@ import com.localcoupon.couponservice.store.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public interface StoreRepository extends JpaRepository<Store, Long> {
+    Optional<Store> findByOwnerId(Long ownerId);
     List<Store> findByOwnerIdAndIsDeletedFalse(Long ownerId);
     @Query("""
         SELECT s
