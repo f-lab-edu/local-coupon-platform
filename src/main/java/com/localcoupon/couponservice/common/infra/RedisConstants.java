e4ec165e04ec291bc68997afafdfa8c1119c689c
sdj3261 <tlsehdwo7@naver.com>
2025-07-03 14:58:24 +0900

    chore: feature3 및 feature5 작업 커밋 정리
    - 중복 커밋 제거
    - 쿠폰 Redis 캐시 기능 추가
    - 코드 리팩토링 및 테스트 코드 작성

---
+++ src/main/java/com/localcoupon/couponservice/common/infra/RedisConstants.java
@@ -0,0 +1,12 @@
+package com.localcoupon.couponservice.common.infra;
+
+import java.time.Duration;
+
+
+public final class RedisConstants {
+    public static final Duration SESSION_TTL = Duration.ofHours(1);
+    public static final String SESSION_PREFIX = "SESSION:";
+    public static final String COUPON_READY_PREFIX = "coupon:ready:";
+    public static final String COUPON_OPEN_PREFIX = "coupon:open:";
+}
+
