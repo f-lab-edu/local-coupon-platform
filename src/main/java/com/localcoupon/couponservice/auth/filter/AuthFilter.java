613d366bd791f4af32c5e3dd242e16b8d8918ca9
sdj3261 <tlsehdwo7@naver.com>
2025-07-05 19:00:32 +0900

    [#5] build : redissonConfig 설정 추가

--- src/main/java/com/localcoupon/couponservice/auth/filter/AuthFilter.java
+++ src/main/java/com/localcoupon/couponservice/auth/filter/AuthFilter.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.localcoupon.couponservice.auth.dto.UserSessionDto;
 import com.localcoupon.couponservice.auth.security.CustomUserDetails;
 import com.localcoupon.couponservice.common.config.SecurityConfig;
-import com.localcoupon.couponservice.common.util.RedisUtils;
+import com.localcoupon.couponservice.common.infra.RedisConstants;
 import com.localcoupon.couponservice.common.util.StringUtils;
 import com.localcoupon.couponservice.user.enums.UserErrorCode;
 import jakarta.servlet.FilterChain;
@@ -65,7 +65,7 @@ public class AuthFilter extends OncePerRequestFilter {
 
 
     private UsernamePasswordAuthenticationToken validateSession(String sessionId) {
-        String json = redisTemplate.opsForValue().get(RedisUtils.SESSION_PREFIX + sessionId);
+        String json = redisTemplate.opsForValue().get(RedisConstants.SESSION_PREFIX + sessionId);
         if (StringUtils.isEmpty(json)) {
             throw new InsufficientAuthenticationException(UserErrorCode.USER_NOT_FOUND.getMessage());
         }
