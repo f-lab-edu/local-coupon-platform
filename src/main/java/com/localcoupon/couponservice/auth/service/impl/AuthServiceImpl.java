1d3cfaef1c467077896e06dc9549300243a49d4b
sdj3261 <tlsehdwo7@naver.com>
2025-07-16 21:03:05 +0900

    [#1] test : Auth 관련 테스트코드 재작성, @MockBean Deprecated 대응 및 부자연스러운 테스트 수정 작업

--- src/main/java/com/localcoupon/couponservice/auth/service/impl/AuthServiceImpl.java
+++ src/main/java/com/localcoupon/couponservice/auth/service/impl/AuthServiceImpl.java
@@ -1,17 +1,13 @@
 package com.localcoupon.couponservice.auth.service.impl;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.localcoupon.couponservice.auth.dto.UserSessionDto;
 import com.localcoupon.couponservice.auth.dto.request.LoginRequestDto;
 import com.localcoupon.couponservice.auth.dto.response.LoginResponseDto;
 import com.localcoupon.couponservice.auth.dto.response.LogoutResponseDto;
 import com.localcoupon.couponservice.auth.enums.AuthErrorCode;
 import com.localcoupon.couponservice.auth.exception.PasswordNotMatchException;
+import com.localcoupon.couponservice.auth.repository.SessionRepository;
 import com.localcoupon.couponservice.auth.service.AuthService;
-import com.localcoupon.couponservice.common.enums.CommonErrorCode;
-import com.localcoupon.couponservice.common.exception.CommonException;
-import com.localcoupon.couponservice.common.infra.RedisConstants;
 import com.localcoupon.couponservice.common.util.PasswordEncoder;
 import com.localcoupon.couponservice.common.util.TokenGenerator;
 import com.localcoupon.couponservice.user.entity.User;
@@ -19,17 +15,13 @@ import com.localcoupon.couponservice.user.enums.UserErrorCode;
 import com.localcoupon.couponservice.user.exception.UserNotFoundException;
 import com.localcoupon.couponservice.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
     private final UserRepository userRepository;
-    private final StringRedisTemplate redisTemplate;
-    private final ObjectMapper objectMapper;
+    private final SessionRepository sessionRepository;
 
     @Override
     public LoginResponseDto login(LoginRequestDto request) {
@@ -42,30 +34,14 @@ public class AuthServiceImpl implements AuthService {
 
         String sessionToken = TokenGenerator.createSessionToken();
 
-        // UserSessionDto 생성
-        UserSessionDto sessionDto = new UserSessionDto(
-                user.getId(),
-                user.getEmail(),
-                user.getNickname(),
-                List.of(user.getRole().name())
-        );
-
-        try {
-            redisTemplate.opsForValue().set(
-                    RedisConstants.SESSION_PREFIX + sessionToken,
-                    objectMapper.writeValueAsString(sessionDto),
-                    RedisConstants.SESSION_TTL
-            );
-        } catch (JsonProcessingException e) {
-            throw new CommonException(CommonErrorCode.JSON_SERIALIZE_ERROR);
-        }
+        sessionRepository.save(sessionToken, UserSessionDto.of(user));
 
         return LoginResponseDto.of(sessionToken);
     }
 
     @Override
     public LogoutResponseDto logout(String sessionToken) {
-        redisTemplate.delete(RedisConstants.SESSION_PREFIX + sessionToken);
-        return new LogoutResponseDto(sessionToken);
+        sessionRepository.delete(sessionToken);
+        return LogoutResponseDto.of(sessionToken);
     }
 }
