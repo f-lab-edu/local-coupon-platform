f908525eeab3c108c222b414688216c2fc057aeb
sdj3261 <tlsehdwo7@naver.com>
2025-07-16 21:10:05 +0900

    [#5] feat : 쿠폰 스케줄러 배치 JdbcTemplate -> Jpa 방식으로 변경

--- src/main/java/com/localcoupon/couponservice/coupon/batch/service/CouponClosedBatch.java
+++ src/main/java/com/localcoupon/couponservice/coupon/batch/service/CouponClosedBatch.java
@@ -1,62 +1,66 @@
 package com.localcoupon.couponservice.coupon.batch.service;
 
+import com.localcoupon.couponservice.common.enums.Result;
 import com.localcoupon.couponservice.common.infra.RedisProperties;
-import com.localcoupon.couponservice.coupon.batch.mapper.CouponBatchMapper;
+import com.localcoupon.couponservice.coupon.entity.Coupon;
 import com.localcoupon.couponservice.coupon.repository.CouponRedisRepository;
+import com.localcoupon.couponservice.coupon.repository.CouponRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Clock;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
+@Transactional
 public class CouponClosedBatch {
 
+    private final CouponRepository couponRepository;
     private final CouponRedisRepository couponRedisRepository;
-    private final CouponBatchMapper couponBatchMapper;
     private final RedisProperties redisProperties;
+    private final Clock clock;
 
-    @Scheduled(cron = "0 0 */1 * * *") // 1시간마다 실행
+    @Scheduled(cron = "0 0 */1 * * *")
     public void deleteExpiredCoupons() {
-        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime now = LocalDateTime.now(clock);
 
-        List<Long> expiredCouponIds = couponBatchMapper.findExpiredCouponIds(now);
+        couponRepository.findByCouponIssueEndTimeBefore(now)
+                .forEach(coupon -> {
+                    try {
+                        syncIssuedCountToDb(coupon);
+                        deleteRedisKey(coupon.getId());
+                    } catch (Exception e) {
+                        log.error("[Coupon-Closed-Batch] Failed processing couponId={}", coupon.getId(), e);
+                    }
+                });
 
-        expiredCouponIds.forEach(couponId -> {
-            try {
-                syncIssuedCountToDb(couponId);
-                deleteRedisKey(couponId);
-            } catch (Exception e) {
-                log.error("[Coupon-Closed-Batch] Failed processing couponId={}", couponId, e);
-            }
-        });
     }
 
-    private void syncIssuedCountToDb(Long couponId) {
-        String redisKey = redisProperties.couponOpenPrefix() + couponId;
+    private Result syncIssuedCountToDb(Coupon coupon) {
+        String redisKey = redisProperties.couponOpenPrefix() + coupon.getId();
 
-        couponRedisRepository.getValue(redisKey, String.class)
-                .map(Long::parseLong)
-                .ifPresentOrElse(
-                        issuedCount -> {
-                            int updatedRows = couponBatchMapper.updateIssuedCount(couponId, issuedCount);
-                            log.info("[Coupon-Closed-Batch] couponId={} , issuedCount={} → DB 업데이트 ({} rows)",
-                                    couponId, issuedCount, updatedRows);
-                        },
-                        () -> log.info("[Coupon-Closed-Batch] No issuedCount found in Redis for couponId={}", couponId)
-                );
+        return couponRedisRepository.getValue(redisKey, String.class)
+                .map(Integer::parseInt)
+                .map(issuedCount -> {
+                    coupon.syncIssuedCount(issuedCount);
+
+                    log.info("[Coupon-Closed-Batch] couponId={} , issuedCount={} → DB 업데이트",
+                            coupon.getId(), issuedCount);
+                    return Result.SUCCESS;
+                })
+                .orElse(Result.FAIL);
     }
 
-
-        private boolean deleteRedisKey(Long couponId) {
-            if (couponRedisRepository.deleteData(redisProperties.couponOpenPrefix() + couponId)) {
-                log.info("[Coupon-Closed-Batch] Deleted Redis couponId: {}", couponId);
-                return true;
-            }
-            return false;
+    private boolean deleteRedisKey(Long couponId) {
+        if (couponRedisRepository.deleteData(redisProperties.couponOpenPrefix() + couponId)) {
+            log.info("[Coupon-Closed-Batch] Deleted Redis couponId: {}", couponId);
+            return true;
+        }
+        return false;
     }
 }
