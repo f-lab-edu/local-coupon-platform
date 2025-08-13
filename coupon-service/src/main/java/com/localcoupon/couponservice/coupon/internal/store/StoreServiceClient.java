b895128c7eedef26d5ea71ae137afefb55a5d84a
sdj3261 <tlsehdwo7@naver.com>
2025-08-13 23:53:52 +0900

    [#9] feat : 쿠폰 서비스 발급 시 @Async에서 @TransactionListener로 후처리 변경

--- coupon-service/src/main/java/com/localcoupon/couponservice/coupon/internal/store/StoreServiceClient.java
+++ coupon-service/src/main/java/com/localcoupon/couponservice/coupon/internal/store/StoreServiceClient.java
@@ -1,15 +1,17 @@
 package com.localcoupon.couponservice.coupon.internal.store;
 
-import com.localcoupon.couponservice.coupon.internal.store.dto.StoreSummaryDto;
+import com.localcoupon.couponservice.coupon.common.contract.store.StoreResponseDto;
+import com.localcoupon.couponservice.coupon.common.dto.response.SuccessResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(
-        name = "storeClient"
+        name = "storeClient",
+        url = "${services.other.base-url}"
 )
 public interface StoreServiceClient {
 
-    @GetMapping("/api/v1/stores/my")
-    StoreSummaryDto getMyStore(@RequestHeader("X-USER-ID") Long userId);
+    @GetMapping("/stores/my")
+    SuccessResponse<StoreResponseDto> getMyStore(@RequestHeader("X-USER-ID") Long userId);
 }
