f00f27d623a3b55eaca9911361e8b4d88ae6ee77
sdj3261 <tlsehdwo7@naver.com>
2025-08-13 23:54:48 +0900

    Revert "[#9] feat : 쿠폰 서비스 FeignHeaderRelay를 통해 서비스간 인증헤더 삽입 인터셉터 기능 개발"
    This reverts commit 9693cae9699ebd4cb682aa942f0e2fb04dd76ec8.

--- coupon-service/src/main/java/com/localcoupon/couponservice/coupon/internal/store/StoreServiceClient.java
+++ coupon-service/src/main/java/com/localcoupon/couponservice/coupon/internal/store/StoreServiceClient.java
@@ -1,17 +1,15 @@
 package com.localcoupon.couponservice.coupon.internal.store;
 
-import com.localcoupon.couponservice.coupon.common.contract.store.StoreResponseDto;
-import com.localcoupon.couponservice.coupon.common.dto.response.SuccessResponse;
+import com.localcoupon.couponservice.coupon.internal.store.dto.StoreSummaryDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(
-        name = "storeClient",
-        url = "${services.other.base-url}"
+        name = "storeClient"
 )
 public interface StoreServiceClient {
 
-    @GetMapping("/stores/my")
-    SuccessResponse<StoreResponseDto> getMyStore(@RequestHeader("X-USER-ID") Long userId);
+    @GetMapping("/api/v1/stores/my")
+    StoreSummaryDto getMyStore(@RequestHeader("X-USER-ID") Long userId);
 }
--- coupon-service/src/main/java/com/localcoupon/couponservice/coupon/internal/user/UserServiceClient.java
+++ coupon-service/src/main/java/com/localcoupon/couponservice/coupon/internal/user/UserServiceClient.java
@@ -1,17 +1,15 @@
 package com.localcoupon.couponservice.coupon.internal.user;
 
-import com.localcoupon.couponservice.coupon.common.contract.store.StoreResponseDto;
-import com.localcoupon.couponservice.coupon.common.dto.response.SuccessResponse;
+import com.localcoupon.couponservice.coupon.internal.user.dto.UserSummaryDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 // services.user.base-url=yaml에서 주입 (게이트웨이나 직접 주소)
 @FeignClient(
-        name = "userClient",
-        url = "${services.other.base-url}"
+        name = "userClient"
 )
 public interface UserServiceClient {
-    @GetMapping("/users/me")
-    SuccessResponse<StoreResponseDto> getById(@RequestHeader("X-USER-ID") Long id);
+    @GetMapping("/api/v1/users/me")
+    UserSummaryDto getById(@RequestHeader("X-USER-ID") Long id);
 }
